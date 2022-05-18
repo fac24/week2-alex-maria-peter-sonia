ff933cd860cf10c7d86a2ec9d57e4961d9f312cc
= <maria.louisa.castro@gmail.com>
2022-05-18 16:18:10 +0100

    Add more inputs to the form
    Co-authored: Peter <PJSalter@users.noreply.github.com>

--- routes/home.js
+++ routes/home.js
@@ -1,19 +1,32 @@
 function get(request, response) {
   const form = `
-    <form method="POST">
+    <form method="POST" action="/add-posts">
       <label for="username">Username:</label>
-      <input id="username" name="username" type="text" />
+      <input id="username" name="username" type="text" required />
+        <br />
       <label for="age">Age:</label>
       <input id="age" name="age" type="number" />
+        <br />
       <label for="base_flavour">Ice cream flavour:</label>
       <input id="base_flavour" name="base_flavour" type="text" />
-      <label for="topping">Topping:</label>
+        <br />
+      <label for="fandom">Fandom rating:</label>
+      <input type="range" id="fandom" name="fandom" min="1" max="5" value="1">
+         <br />
+      <label for="topping">Toppings:</label>
       <select id="topping" name="topping">
         <option value="sprinkles">Sprinkles</option>
         <option value="flake">Chocolate flake</option>
         <option value="rasp_syrup">Raspberry syrup</option>
         <option value="choc_syrup">Chocolate syrup</option>
+        <option value="hot_fudge">Hot fudge</option>
+        <option value="marshmellow">Melted marshmellow</option>
+        <option value="none">None</option>
       </select>
+         <br />
+      <label for="comment">Comment:</label>
+      <textarea id="comment" name="comment"></textarea>
+         <br />
       <button type="submit" class="btn">Submit</button>
     </form>
   `;
