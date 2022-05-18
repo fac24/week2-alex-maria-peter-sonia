54d1d001f85429867fa353e95b2718b51e7f8936
sonianb <sonia.botsarova@gmail.com>
2022-05-18 18:42:13 +0100

    fix typos

--- database/init.sql
+++ database/init.sql
@@ -1,6 +1,6 @@
-BEGIN 
+BEGIN;
 
-DROP TABLE IF EXISTS ;
+DROP TABLE IF EXISTS users, ice_cream_posts CASCADE;
 
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
@@ -21,14 +21,14 @@ INSERT INTO users (username, age, fandom) VALUES
   ('gelatofiend ', 19, 5),
   ('apatheticAlan', 57, 2),
   ('misscream', 28, 4),
-  ('pringles_fan', 42, 1),
+  ('pringles_fan', 42, 1)
 ;
 
 INSERT INTO ice_cream_posts (user_id, base_flavour, topping, comment) VALUES
   (1, 'strawberry', 'sprinkles', 'Give me sprinkles or give me death'),
   (2, 'pistachio','hot fudge', 'pistachio is fine, I guess'),
   (3, 'caramel', 'melted marshmallow', 'Nothing better than a caramel ice cream after a hard day of coding.'),
-  (4, 'vanilla', 'none', 'Why are you all talking about ice cream? I came here for the crisps.', )
+  (4, 'vanilla', 'none', 'Why are you all talking about ice cream? I came here for the crisps.' )
 ;
 
 COMMIT;
