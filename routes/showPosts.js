c1682cc69157ba74e236a393c8c7c722cad560f3
sonianb <sonia.botsarova@gmail.com>
2022-05-19 10:24:47 +0100

    display comment and age

--- routes/showPosts.js
+++ routes/showPosts.js
@@ -1,13 +1,16 @@
 const db = require("../database/connection.js");
 
 function get(request, response) {
-    db.query(`SELECT users.username, users.age, ice_cream_posts.base_flavour 
+    db.query(`SELECT users.username, users.age, ice_cream_posts.base_flavour, ice_cream_posts.comment 
     FROM users
     INNER JOIN ice_cream_posts
     ON users.id = ice_cream_posts.user_id`).then((result) => {
         const posts = result.rows;
         console.log(posts);
-        const postList = posts.map((post) => `<li>${post.username} likes ${post.base_flavour}</li>`).join("");
+        const postList = posts.map((post) => 
+        `<li class="user-post">${post.username}, ${post.age} likes ${post.base_flavour} ice cream.
+        ${post.username} said: ${post.comment}!      
+        </li>`).join("");
         response.send(`<ul>${postList}</ul>`);
     });
 }
