54aa0125b5c1dcffa2a29fd7eeabf3e2d5dcf1bd
= <maria.louisa.castro@gmail.com>
2022-05-19 15:21:26 +0100

    Add more posts info to thr page

--- routes/showPosts.js
+++ routes/showPosts.js
@@ -1,20 +1,32 @@
 const db = require("../database/connection.js");
 
-
 function get(request, response) {
-    db.query(`SELECT users.username, users.age, ice_cream_posts.base_flavour, ice_cream_posts.comment 
+  db.query(
+    `SELECT users.username, users.age, users.fandom, ice_cream_posts.base_flavour, ice_cream_posts.comment 
     FROM users
     INNER JOIN ice_cream_posts
-    ON users.id = ice_cream_posts.user_id`).then((result) => {
-        const posts = result.rows;
-        const postList = posts.map((post) => 
-        `<li class="user-post">${post.username}, ${post.age}!      
-        </li>`).join("");
-        response.send(`<ul>${postList}</ul>`);
-    });
+    ON users.id = ice_cream_posts.user_id`
+  ).then((result) => {
+    let postsHTML = "";
+    const posts = result.rows;
+    console.log(posts);
+    const postList = posts.map(
+      (post) =>
+        (postsHTML += `
+        <div class="post-container">
+        <p>Username: ${post.username}  <span>${post.fandom}⭐</span></p>
+        <p>Loves: ${post.base_flavour}</p>
+        <p>Comment: ${post.comment}</p>
+        </div>
+        `)
+    );
+    // `<li class="user-post">${post.username}, ${post.age}!
+    // </li>`).join("");
+    response.send(`${postsHTML}`);
+  });
 }
 
-module.exports = {get};
+module.exports = { get };
 
 // likes ${post.base_flavour} ice cream.
 //         ${post.username} said: ${post.comment}
