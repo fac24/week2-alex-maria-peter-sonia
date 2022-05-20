74830976874aa4482ce2b5832dec5b58b42a70d2
= <maria.louisa.castro@gmail.com>
2022-05-20 10:55:30 +0100

    Add link to home

--- routes/showPosts.js
+++ routes/showPosts.js
@@ -6,38 +6,45 @@ function get(request, response) {
     FROM users
     INNER JOIN ice_cream_posts
     ON users.id = ice_cream_posts.user_id`
-  ).then((result) => {
-    let postsHTML = "";
-    const posts = result.rows;
-    posts.map(
-      (post) =>
-        (postsHTML += `
+  )
+    .then((result) => {
+      let postsHTML = "";
+      const posts = result.rows;
+      posts.map(
+        (post) =>
+          (postsHTML += `
         <div class="post-container">
         <p>User: ${post.username}  <span>⭐${post.fandom}</span></p>
         <p>Loves: ${post.base_flavour}</p>
         <p>Comment: ${post.comment}</p>
         </div>
         `)
-    );
-    response.send(`
+      );
+      response.send(`
 <!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="style.css" />
     <title>iScream</title>
   </head>
   <body>
+    <h1>I-Scream</h1>
+    <a href="/">Click here to add a post!</a>
+
     ${postsHTML}
   </body>
 </html>
-`)
-  })
-      .catch((error) => {
-        console.error(error); 
-        response.status(500).send("<h1>Problem loading page.</h1>");
+`);
+    })
+    .catch((error) => {
+      console.error(error);
+      response.status(500).send("<h1>Problem loading page.</h1>");
     });
 }
 
