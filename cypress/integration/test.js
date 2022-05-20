e21919975c3ee9ee651bf6982ce9d8babc42af12
sonianb <sonia.botsarova@gmail.com>
2022-05-20 11:53:18 +0100

    add after test

--- cypress/integration/test.js
+++ cypress/integration/test.js
@@ -1,9 +1,8 @@
-describe('', () => {
+beforeEach(() => {
+  cy.task("resetDb");
+});
 
-  beforeEach(() => {
-    cy.task("resetDb");
-  });
-
+describe('can create an ice cream post', () => {
   it('create a new ice cream post', () => {
     cy.visit("/");
     cy.get("input[name='username']").type("icefairy");
@@ -17,10 +16,15 @@ describe('', () => {
     cy.contains("icefairy");
     cy.contains("I hate pringles.");
   });
+});
 
+describe("reset DB", () => {
   it("db has been reset", () => {
     cy.visit("http://localhost:3000/show-posts");
     cy.get(".post-container").should('have.length', 4);
   });
 });
 
+after(() => {
+  cy.task("resetDb");
+});
